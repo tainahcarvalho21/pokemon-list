97f5d0f6a657699038bd17630593358f4747f81d
tainahcarvalho21 <tainah_carvalho@hotmail.com>
2023-09-27 15:18:13 -0300

    load more event

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
-function convertPokemonToLi(pokemon) {
-    return `
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 6
+let offset = 0;
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+                const newHtml = pokemons.map((pokemon) => `
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
@@ -12,11 +18,14 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}" alt="${pokemon.name}">
             </div>
         </li>
-    `
+    `).join('')
+        pokemonList.innerHTML += newHtml
+    })
 }
 
-const pokemonList = document.getElementById('pokemonList')
+loadPokemonItens(offset, limit)
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
